c7ffb44c2166804ff596d77c2139d81d19b03188
Santiago Oggioni <sh.oggioni@gmail.com>
2025-07-07 02:50:35 -0300

    Agrego modulo jugar y sus dependencias

---
+++ Config/CONSTANTES.py
@@ -0,0 +1,18 @@
+
+#Pantalla
+
+ANCHO = 480
+ALTO = 800
+FPS = 60
+
+#Colores
+
+BLANCO = (255, 255, 255)
+NEGRO = (0, 0, 0)
+AMARILLO =  (255, 255 ,0)
+ROJO = (255 , 0,0)
+
+#Tiempo
+SEGUNDO = 1000
+
+TIEMPO_NUEVO_MURCIELAGO = 5
