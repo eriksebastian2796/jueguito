f64cabd4b948ae97bc5b6a130e19fb9411ebfdf2
Santiago Oggioni <sh.oggioni@gmail.com>
2025-07-08 17:38:27 -0300

    Intro animada Erik

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pygame
 import pygame.mixer
-from menu import mostrar_menu
 from Config.CONSTANTES import ANCHO, ALTO
+from intro_animada import intro_con_animacion
+from menu import mostrar_menu
+
 
 pygame.init()
 pygame.mixer.init()
@@ -12,6 +14,7 @@ pantalla = pygame.display.set_mode((ANCHO, ALTO))
 pygame.display.set_caption("Monster hunter")
 pygame.display.set_icon(icono)
 
+intro_con_animacion(pantalla)
 mostrar_menu(pantalla)
 
 pygame.quit()
