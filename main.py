2d5ab7a59aa0a22663cbd51e429e794c1f178dcb
Erik Ramirez <erikramirez2796@gmail.com>
2025-07-08 15:30:18 -0300

    agregue intro de entrada

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import pygame
 pygame.init()
+pygame.mixer.init()
+from intro_animada import intro_con_animacion
 from menu import mostrar_menu
 from constantes import ANCHO, ALTO
 
 pantalla = pygame.display.set_mode((ANCHO, ALTO))
 
-mostrar_menu(pantalla)
+intro_con_animacion(pantalla)   # ⬅ Mostrar la animación primero
+mostrar_menu(pantalla)  # ⬅ Ir al menú luego
+
 pygame.quit()
